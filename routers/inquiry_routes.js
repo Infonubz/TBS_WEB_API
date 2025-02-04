76aab787dcaf4ce48ad21f586feb27e9387a2f61
nbz12 <nbz12@akr.com>
2025-02-04 14:12:29 +0530

    latest code

--- routers/inquiry_routes.js
+++ routers/inquiry_routes.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { postInquiry } = require('../controllers/inquiry_support')
+const { postInquiry } = require('../controllers/inquiry_support');
+const { authenticateToken } = require('../middileware/Auth');
 
 
 const inquiryRouter = express.Router()
 
-inquiryRouter.post('/send-inquiry', postInquiry);
+inquiryRouter.post('/send-inquiry', authenticateToken, postInquiry);
 
 module.exports = { inquiryRouter }
